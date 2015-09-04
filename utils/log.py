b21e0f76ebaa5322f9a44d1a47d048a2e653307d
Eddy Hintze <eddy.hintze@gmail.com>
2015-09-04 11:34:43 -0400

    Only log errors

--- utils/log.py
+++ utils/log.py
@@ -6,9 +6,9 @@ def setup_custom_logger(name, log_file):
     # Create logger
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
-    # Create file handler which logs even debug messages
+    # Create file handler which logs error messages
     fh = logging.FileHandler(log_file)
-    fh.setLevel(logging.DEBUG)
+    fh.setLevel(logging.ERROR)
     # Create console handler with a higher log level
     ch = logging.StreamHandler()
     ch.setLevel(logging.CRITICAL)
